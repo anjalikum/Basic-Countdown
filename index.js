0d13a634ae7a41343ea8462c37c8192e578dd14c
anjalikum <51411674+anjalikum@users.noreply.github.com>
2019-06-28 15:47:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ var x = setInterval(function() {
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
+  // Time calculations for weeks, days, hours, minutes and seconds
+  var weeks = Math.floor(distance / (1000 * 60 * 60 *24 * 7));
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("demo").innerHTML = weeks + "w " + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is over, write some text
