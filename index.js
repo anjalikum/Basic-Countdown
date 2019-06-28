9caf83734c53c5a1fe4e4ecf3ad5190eda8ec682
anjalikum <51411674+anjalikum@users.noreply.github.com>
2019-06-28 15:24:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 15, 2019 09:02:00").getTime();
+var countDownDate = new Date("Jul 30, 2019 09:02:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
